ab1505710ce7fbe6fbdd2ed603b6f33b66f3de66
Ihor Poliakov <piv2003@gmail.com>
2023-01-19 23:25:02 +0200

    fixing bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,20 @@
 import { refs } from './refs';
-import { getData } from './getData';
+import api, { getData } from './getData';
 import { markupCard } from './markup';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 let gallery = new SimpleLightbox('.gallery a');
-let value = '';
-let page = 1;
-
-
 
 const guard = document.querySelector('.js-guard');
-
-
 const options = {
   root: null,
   rootMargin: '300px',
-  threshold: 0
-}
+  threshold: 0,
+};
 
 let totalImg = 0;
-let totalPage = 0; 
-// const observer = new IntersectionObserver(infiniteScroll, options);
 
 async function onSubmit(evt) {
   evt.preventDefault();
@@ -35,7 +27,7 @@ async function onSubmit(evt) {
   } else {
     refs.gallery.innerHTML = '';
   }
-  
+
   const data = await getData(value, page);
   if (data.totalHits === 0) {
     Notiflix.Notify.failure(
@@ -43,28 +35,13 @@ async function onSubmit(evt) {
     );
     return;
   }
+
   evt.target.reset();
   refs.loadMore.classList.remove('is-hidden');
   refs.gallery.insertAdjacentHTML('beforeend', markupCard(data.hits));
   Notiflix.Notify.success(`Hooray! We found ${data.total} images..`);
 
-  totalPage = Math.ceil(data.total/40);
-  // gallery.refresh();
-
-  // observer.observe(refs.guard);
-  // const markup = createMarkup(data.hits);
-  // totalImg += data.hits.length;
-  // refs.gallery.insertAdjacentHTML('beforeend', markup);
-  
-  
-
-  // if (totalImg >= data.totalHits) {
-  //   observer.unobserve(refs.guard);
-  //   Notiflix.Notify.warning(
-  //     "We're sorry, but you've reached the end of search results."
-  //   );
-  // }
-  
+  totalPage = Math.ceil(data.total / data.hits.length);  
 }
 
 async function loadMoreData() {
@@ -72,33 +49,34 @@ async function loadMoreData() {
   const data = await getData(value, page);
   //Math.ceil - rounds the argument to the nearest higher integer
   totalImg += data.hits.length;
-  if ( totalPage < page) {
+  if (totalPage < page) {
     refs.loadMore.classList.add('is-hidden');
     Notiflix.Notify.info(
       "We're sorry, but you've reached the end of search results."
     );
-  }  
-  refs.gallery.insertAdjacentHTML('beforeend', markupCard(data.hits));  
-  
+  }
+  refs.gallery.insertAdjacentHTML('beforeend', markupCard(data.hits));
+
   gallery.refresh();
-  
+
   const { height: cardHeight } = document
     .querySelector('.gallery')
     .firstElementChild.getBoundingClientRect();
+    //returns the size of the element and its position relative to the viewport
 
-  window.scrollBy({
-    top: cardHeight * 2,
+  window.scrollBy({//scrolling the screen up three rows of images
+    top: cardHeight * 3,
     behavior: 'smooth',
   });
 }
 
-function infiniteScroll(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      onSubmit();
-    }
-  });
-}
+// function infiniteScroll(entries, observer) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       onSubmit();
+//     }
+//   });
+// }
 
 refs.form.addEventListener('submit', onSubmit);
 refs.loadMore.addEventListener('click', loadMoreData);
